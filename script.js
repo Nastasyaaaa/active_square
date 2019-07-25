fb581a6437b8356fd6a8163b0c4b70d332360b33
= <=>
2019-07-25 17:48:21 +0300

    altered solution with delegation

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function makeActive(square)
 {
-	square.style.backgroundColor = 'red';
+	square.classList.add('active');
+}
+
+function makeUnActive(square)
+{
+	square.classList.remove('active');
 }
 
 function isSquare(element)
@@ -8,14 +13,22 @@ function isSquare(element)
 	return element.classList.contains('square');
 }
 
-var list = document.getElementById('list');
-
-list.addEventListener('click', function(event){
+function onClick(event){
 	var square = event.target;
 
 	if(!isSquare(square)){
 		return;
 	}
 
+	if(activeSquare && activeSquare != square){
+		makeUnActive(activeSquare);
+	}
 
-});
+	makeActive(square);
+	activeSquare = square;
+}
+
+var list = document.getElementById('list');
+var activeSquare;
+
+list.addEventListener('click', onClick);
